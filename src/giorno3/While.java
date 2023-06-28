42596bd0aaa20e87c48de2e7a45525d69748e0f8
Tyorna <francescaarata@live.it>
2023-06-28 16:35:25 +0200

    es

--- src/giorno3/While.java
+++ src/giorno3/While.java
@@ -6,20 +6,31 @@ public class While {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		prova();
+	}
+	
+	public static void dividi(String car) {
+		int i= 0;
+		if(car.equals(":q")) {
+			System.out.println("Hai finito!");
+		} else {
+		
+		while (i < car.length()) {
+	    System.out.println(car.charAt(i) + ",");
+	    i++;
+	} 
+		prova();
+		}
+		
+	}
+	
+	public static void prova() {
 		Scanner caratteri = new Scanner(System.in);
 		System.out.println("Separa la tua frase in lettere");
 		String parola = caratteri.nextLine();
 		dividi(parola);
 		caratteri.close();
-	}
-	
-	public static void dividi(String car) {
-		int i= 0;
-		while (i < car.length()) {
-	    System.out.println(car.charAt(i) + ",");
-	    i++;
-	} 
-	}
+		}
 	
 }
 	
